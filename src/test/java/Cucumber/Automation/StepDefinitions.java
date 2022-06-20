142e0d61a2c9a685dd525e1ad5a1cd9bf7a97b64
Yoko Alpha <yokoo.alpha@gmail.com>
2022-06-20 21:11:19 +0100

    Added scenario for testing login page.

--- src/test/java/Cucumber/Automation/StepDefinitions.java
+++ src/test/java/Cucumber/Automation/StepDefinitions.java
@@ -1,5 +1,8 @@
 package Cucumber.Automation;
 
+import java.util.List;
+
+import io.cucumber.datatable.DataTable;
 import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
@@ -8,22 +11,31 @@ import io.cucumber.java.en.When;
 public class StepDefinitions {
 	@Given("User is on landing page") 
 	public void User_is_on_landing_page()throws Throwable {
-		System.out.print("Navigate to login url. ");
+		System.out.println("Navigate to login url.");
 	}
 
 	@When("^User logs into application with username \"([^\"]*)\" and password \"([^\"]*)\"$")
 	public void User_logs_into_application_with_username_and_password(String username, String password)throws Throwable {
-		System.out.print("User: "+username);
-		System.out.print(" Password: "+password+". ");
+		System.out.println("User: "+username);
+		System.out.println("Password: "+password);
 	}
 
 	@Then("^Home page is populated$")
 	public void Home_page_is_populated()throws Throwable {
-		System.out.print("Validated home page. ");	
+		System.out.println("Validated home page.");	
 		}
 	
 	@And("^Cards displayed are \"([^\"]*)\"$")
 	public void Cards_displayed_are(String arg1)throws Throwable {
-		System.out.print("Cards displayed are " +arg1+". ");	
+		System.out.println("Cards displayed are "+arg1+".");	
 		}
+	@When("^User sign up with following details$")
+	public void User_sign_up_with_following_details(DataTable data)throws Throwable {
+		List<List<String>> obj = data.asLists();
+		System.out.println(obj.get(0).get(0));
+		System.out.println(obj.get(0).get(1));
+		System.out.println(obj.get(0).get(2));
+		System.out.println(obj.get(0).get(3));
+		System.out.println(obj.get(0).get(4));
+	}
 }
--- src/test/resources/Cucumber/Automation/Login.feature
+++ src/test/resources/Cucumber/Automation/Login.feature
@@ -11,3 +11,10 @@ Feature: Application login
     When User logs into application with username "Jerry" and password "345"
     Then Home page is populated
     And Cards displayed are "false"
+    
+  Scenario: Home page default login
+    Given User is on landing page
+    When User sign up with following details
+    |Jenny |123|jenny@gmail.com|London|zy1 9kl|
+    Then Home page is populated
+    And Cards displayed are "false"
